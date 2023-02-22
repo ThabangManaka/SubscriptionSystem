8c5c44d0d825dba5fd38a1cbaab6659b0dd31376
ThabangManaka <thabangdansteyn@gmail.com>
2023-02-22 05:46:39 +0200

    Added Components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,10 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { SubscriptionComponent } from './subscription/subscription.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    SubscriptionComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
